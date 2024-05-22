95e67abc3cfd38cd1c48f6e04f563af5b792410b
Axel Brisse <axel.brisse@gmail.com>
2024-05-23 01:47:40 +0200

    cleanup

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 run:
-	cabal run -v0
-	dot files.dot -Tpng > files.png
-	eog files.png 2>/dev/null
+	@cabal run -v0
+	@dot files.dot -Tpng > files.png
+	@eog files.png 2>/dev/null
+
+install:
+	cabal update && cabal install --overwrite-policy=always
+
+clean:
+	rm -rf build dist-newstyle .cabal* .ghc.environment*
--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,28 @@
+{-# LANGUAGE TupleSections #-}
+
 module Main where
 
 import qualified Data.GraphViz as G
 import qualified Data.GraphViz.Attributes.Complete as G
-import qualified Data.GraphViz.Types as G
-
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TL
-import System.FilePath.Posix
-import System.Directory
-import System.IO
+import System.Directory (doesFileExist, getSymbolicLinkTarget, listDirectory, pathIsSymbolicLink)
+import System.FilePath.Posix (normalise)
 
+rootDir :: String
 rootDir = "tmp"
 
-data VLabel = VLDirectory
-            | VLSymlink
-            | VLFile
-            deriving (Show)
+data VLabel
+  = VLDirectory
+  | VLSymlink
+  | VLFile
+  deriving (Show)
 type V = (FilePath, VLabel)
 
-data ELabel = ELHardlink
-            | ELSymlink
-            deriving (Show)
+data ELabel
+  = ELHardlink
+  | ELSymlink
+  deriving (Show)
 type E = (FilePath, FilePath, ELabel)
 
 type FileGraph = ([V], [E])
@@ -35,42 +37,43 @@ readDirectoryGraph root = do
       if isFile
         then onFile
         else onDirectory
-  where
-    onSymlink :: IO FileGraph
-    onSymlink = do
-      target <- normalise <$> getSymbolicLinkTarget root
-      return ([(root, VLSymlink)], [(root, target, ELSymlink)])
-      
-    onFile :: IO FileGraph
-    onFile = do
-      return ([(normalise root, VLFile)], [])
-    
-    onDirectory :: IO FileGraph
-    onDirectory = do
-      children <- map (\x -> root ++ "/" ++ x) <$> listDirectory root :: IO [FilePath]
-      subgraphs <- mapM readDirectoryGraph children :: IO [FileGraph]
-      let v = (normalise root, VLDirectory) :: V
-          es = map (\x -> (normalise root, x, ELHardlink)) children :: [E]
-          childVertices = concatMap fst subgraphs :: [V]
-          childEdges = concatMap snd subgraphs :: [E]
-      return (v:childVertices, es ++ childEdges)
+ where
+  onSymlink :: IO FileGraph
+  onSymlink = do
+    target <- normalise <$> getSymbolicLinkTarget root
+    return ([(root, VLSymlink)], [(root, target, ELSymlink)])
+
+  onFile :: IO FileGraph
+  onFile = do
+    return ([(normalise root, VLFile)], [])
+
+  onDirectory :: IO FileGraph
+  onDirectory = do
+    children <- map (\x -> root ++ "/" ++ x) <$> listDirectory root :: IO [FilePath]
+    subgraphs <- mapM readDirectoryGraph children :: IO [FileGraph]
+    let v = (normalise root, VLDirectory) :: V
+        es = map (normalise root,,ELHardlink) children :: [E]
+        childVertices = concatMap fst subgraphs :: [V]
+        childEdges = concatMap snd subgraphs :: [E]
+    return (v : childVertices, es ++ childEdges)
 
 fileGraphParams :: G.GraphvizParams FilePath VLabel ELabel () VLabel
-fileGraphParams = G.defaultParams {
-  G.fmtNode = \(v, vl) -> case vl of
-      VLDirectory -> colorAttribute $ G.RGB 0 0 0
-      VLSymlink   -> colorAttribute $ G.RGB 40 255 40
-      VLFile      -> colorAttribute $ G.RGB 255 40 40,
-  G.fmtEdge = \(from, to, el) -> case el of
-      ELHardlink -> colorAttribute $ G.RGB 0 0 0
-      ELSymlink  -> colorAttribute $ G.RGB 40 255 40
-      }  
-  where
-    colorAttribute color = [ G.Color $ G.toColorList [ color ] ]
+fileGraphParams =
+  G.defaultParams
+    { G.fmtNode = \(_, vl) -> case vl of
+        VLDirectory -> colorAttribute $ G.RGB 0 0 0
+        VLSymlink -> colorAttribute $ G.RGB 40 255 40
+        VLFile -> colorAttribute $ G.RGB 255 40 40
+    , G.fmtEdge = \(_, _, el) -> case el of
+        ELHardlink -> colorAttribute $ G.RGB 0 0 0
+        ELSymlink -> colorAttribute $ G.RGB 40 255 40
+    }
+ where
+  colorAttribute color = [G.Color $ G.toColorList [color]]
 
 main :: IO ()
 main = do
   (vs, es) <- readDirectoryGraph rootDir
   let dotGraph = G.graphElemsToDot fileGraphParams vs es :: G.DotGraph FilePath
-      dotText = G.printDotGraph dotGraph :: TL.Text
+  let dotText = G.printDotGraph dotGraph :: TL.Text
   TL.writeFile "files.dot" dotText
