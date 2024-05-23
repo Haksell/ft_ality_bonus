c4af1a23433b8d2e550aa312657bd2b6b09bbcd7
Axel Brisse <axel.brisse@gmail.com>
2024-05-23 04:19:09 +0200

    good

--- srcs/main.hs
+++ srcs/main.hs
@@ -1,103 +1,30 @@
-{-# LANGUAGE DeriveFoldable #-}
-{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main (main) where
+module Main where
 
-import qualified Common as C
-import qualified SDL
-
-import Control.Monad (when)
-import Control.Monad.IO.Class (MonadIO)
-import Data.Maybe
-import Prelude hiding (Left, Right)
-
-whileM :: (Monad m) => m Bool -> m ()
-whileM act = do
-  b <- act
-  when b $ whileM act
-
-data Direction = Help | Up | Down | Left | Right
-  deriving (Show, Eq)
-
-data Intent = Render Direction| Quit
-  deriving (Show, Eq)
-
-data AssetMap a = AssetMap
-  { help :: a
-  , up :: a
-  , down :: a
-  , left :: a
-  , right :: a
-  }
-  deriving (Foldable, Traversable, Functor)
-
-type RenderFunction m = (Direction -> m ())
-
-surfacePaths :: AssetMap FilePath
-surfacePaths =
-  AssetMap
-    { help = "./assets/press.bmp"
-    , up = "./assets/up.bmp"
-    , down = "./assets/down.bmp"
-    , left = "./assets/left.bmp"
-    , right = "./assets/right.bmp"
-    }
-
-getKey :: SDL.KeyboardEventData -> Maybe Intent
-getKey (SDL.KeyboardEventData _ SDL.Released _ _) = Nothing
-getKey (SDL.KeyboardEventData _ SDL.Pressed True _) = Nothing
-getKey (SDL.KeyboardEventData _ SDL.Pressed False keysym) = Just $
-  case SDL.keysymKeycode keysym of
-    SDL.KeycodeEscape -> Quit
-    SDL.KeycodeUp -> Render Up
-    SDL.KeycodeDown -> Render Down
-    SDL.KeycodeLeft -> Render Left
-    SDL.KeycodeRight -> Render Right
-    _ -> Render Help
-
-mapEventsToIntents :: [SDL.Event] -> [Intent]
-mapEventsToIntents = mapMaybe (payloadToIntent . SDL.eventPayload)
- where
-  payloadToIntent :: SDL.EventPayload -> Maybe Intent
-  payloadToIntent (SDL.KeyboardEvent k) = getKey k
-  payloadToIntent SDL.QuitEvent = Just Quit
-  payloadToIntent _ = Nothing
-
-appLoop :: (MonadIO m) => RenderFunction m -> m Bool
-appLoop render = do
-  xs <- mapEventsToIntents <$> SDL.pollEvents
-
-  let shouldQuit = Quit `elem` xs
-  if shouldQuit
-    then pure False
-    else do
-      applyIntent `mapM_` xs
-      pure True
- where
-  applyIntent (Render key) = render key
-  applyIntent Quit = pure ()
-
-draw :: (MonadIO m) => SDL.Window -> SDL.Surface -> AssetMap SDL.Surface -> RenderFunction m
-draw w screen assets d = do
-  let x = selectSurface d assets
-  C.renderSurfaceToWindow w screen x
- where
-  selectSurface :: Direction -> AssetMap a -> a
-  selectSurface Help = help
-  selectSurface Up = up
-  selectSurface Down = down
-  selectSurface Left = left
-  selectSurface Right = right
+import Control.Monad (unless)
+import SDL
 
 main :: IO ()
-main = C.withSDL $
-  C.withWindow "Lesson 04" (640, 480) $
-    \w -> do
-      screen <- SDL.getWindowSurface w
-      assets <- mapM SDL.loadBMP surfacePaths
-      let doRender = draw w screen assets
-      doRender Help
-      whileM $ appLoop doRender
-      mapM_ SDL.freeSurface assets
-      SDL.freeSurface screen
+main = do
+  initializeAll
+  window <- createWindow "My SDL Application" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+  appLoop renderer
+  destroyWindow window
+
+appLoop :: Renderer -> IO ()
+appLoop renderer = do
+  events <- pollEvents
+  let isQuitEvent event =
+        case eventPayload event of
+          WindowClosedEvent _ -> True
+          KeyboardEvent keyboardEvent ->
+            case keyboardEventKeyMotion keyboardEvent of
+              Pressed -> keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeEscape
+              Released -> False
+          _ -> False
+  rendererDrawColor renderer $= V4 0 0 255 255
+  clear renderer
+  present renderer
+  unless (any isQuitEvent events) $ appLoop renderer
