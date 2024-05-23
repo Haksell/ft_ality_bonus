5a7ddc96e69fa9a6af06176cfa26b386c5a81f43
Axel Brisse <axel.brisse@gmail.com>
2024-05-23 04:34:47 +0200

    clean sdl app

--- srcs/main.hs
+++ srcs/main.hs
@@ -3,28 +3,48 @@
 module Main where
 
 import Control.Monad (unless)
+import Data.Word (Word8)
 import qualified SDL
+
+color1 :: SDL.V4 Word8
+color1 = SDL.V4 0 0 255 255
+
+color2 :: SDL.V4 Word8
+color2 = SDL.V4 255 0 0 255
 
 main :: IO ()
 main = do
   SDL.initializeAll
-  window <- SDL.createWindow "My SDL Application" SDL.defaultWindow
+  window <- SDL.createWindow "ft_ality" SDL.defaultWindow{SDL.windowInitialSize = SDL.V2 800 800}
   renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
-  appLoop renderer
+  appLoop renderer color1
   SDL.destroyWindow window
 
-appLoop :: SDL.Renderer -> IO ()
-appLoop renderer = do
+appLoop :: SDL.Renderer -> SDL.V4 Word8 -> IO ()
+appLoop renderer currentColor = do
   events <- SDL.pollEvents
-  let isQuitEvent event =
-        case SDL.eventPayload event of
-          SDL.WindowClosedEvent _ -> True
-          SDL.KeyboardEvent keyboardEvent ->
-            case SDL.keyboardEventKeyMotion keyboardEvent of
-              SDL.Pressed -> SDL.keysymKeycode (SDL.keyboardEventKeysym keyboardEvent) == SDL.KeycodeEscape
-              SDL.Released -> False
-          _ -> False
-  SDL.rendererDrawColor renderer SDL.$= SDL.V4 0 0 255 255
+  let color =
+        if any colorToggleEvent events
+          then if currentColor == color1 then color2 else color1
+          else currentColor
+  SDL.rendererDrawColor renderer SDL.$= color
   SDL.clear renderer
   SDL.present renderer
-  unless (any isQuitEvent events) $ appLoop renderer
+  unless (any isQuitEvent events) $ appLoop renderer color
+ where
+  isQuitEvent :: SDL.Event -> Bool
+  isQuitEvent event =
+    case SDL.eventPayload event of
+      SDL.WindowClosedEvent _ -> True
+      SDL.KeyboardEvent keyboardEvent ->
+        case SDL.keyboardEventKeyMotion keyboardEvent of
+          SDL.Pressed -> SDL.keysymKeycode (SDL.keyboardEventKeysym keyboardEvent) == SDL.KeycodeEscape
+          SDL.Released -> False
+      _ -> False
+  colorToggleEvent :: SDL.Event -> Bool
+  colorToggleEvent event =
+    case SDL.eventPayload event of
+      SDL.KeyboardEvent keyboardEvent ->
+        SDL.keyboardEventKeyMotion keyboardEvent == SDL.Pressed
+          && SDL.keysymKeycode (SDL.keyboardEventKeysym keyboardEvent) /= SDL.KeycodeEscape
+      _ -> False
